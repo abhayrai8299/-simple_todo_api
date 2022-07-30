a6e6767f10d72a6a94a3af80a898b3e56dc4f734
Abhay Rai <abhayrai@Abhays-MacBook-Air.local>
2022-07-30 14:50:19 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css"
+export default function App() {
+  const [todo, setTodo] = useState([]);
+  const [text, setText] = useState("");
+  const [editid, setisEditid] = useState("");
+  const [active, setactive] = useState(false);
+  const [toggle,setToggle]=useState(false)
 
-function App() {
+  const Changehandler = (e) => {
+    console.log(e.target.value);
+    setText(e.target.value);
+  };
+
+  const addHandler = (e) => {
+    e.preventDefault();
+    if (text !== "") {
+      if (editid !== "") {
+        setTodo(
+          todo.map((element) => {
+            if (element.id === editid) {
+              console.log(element);
+              return { ...element, taskName: text };
+            }
+            return element;
+          })
+        );
+        setisEditid("");
+        setText("");
+        setactive(false)
+        setToggle(false);
+      } else {
+        setTodo([...todo, { taskName: text, id: Math.random() * 10000 }]);
+        setText("");
+      }
+    } else if(editid!=="" )
+      {
+        setToggle(true)
+        setTimeout(()=>{
+          setToggle(false)
+          setactive(true);
+        },2000)
+      }
+      else
+      {
+        setToggle(true)
+        setTimeout(()=>{
+          setToggle(false)
+        },2000)
+      }
+  };
+  const deleteHandler = (id) => {
+    console.log(id);
+    setTodo(todo.filter((item) => item.id !== id));
+    console.log("ssad");
+    if (editid !== "") {
+      setText("");
+    }
+    setisEditid("");
+    setactive(false);
+  };
+  const edit = (id) => {
+    const data = todo.find((element) => {
+      return element.id === id;
+    });
+    console.log(data);
+    setText(data.taskName);
+    setisEditid(id);
+    setactive(true);
+  };
+  const clickHandler = () => {
+    setText("");
+    setisEditid("");
+    setactive(false);
+  };
+  // const editHandler = (id) => {
+  //   console.log("scdsd", text);
+  //   setTodo(
+  //     todo.map((element) => {
+  //       if (element.id === id) {
+  //         console.log(element);
+  //         return { ...element, taskName: text };
+  //       }
+  //       return element;
+  //     })
+  //   );
+  // };
+
+  const fetchData = async () => {
+    const response = await axios
+      .get("https://62e010f4fa8ed271c47dc10e.mockapi.io/task")
+      .catch((e) => {
+        console.log(e);
+      });
+    setTodo(response.data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Todo_Api</h2>
+      <input type="text" className={toggle===true?"addred":"addblank"} onChange={Changehandler} value={text} />
+      <button onClick={addHandler} >{!active ? "Add" : "Update"}</button>
+      {active ? (
+        <span className="cancelbtn" onClick={clickHandler}>
+          Cancel
+        </span>
+      ) : (
+        ""
+      )}
+      <>
+        {todo.map((item) => {
+          return (
+            <li key={item.id}>
+              <ul>
+                {item.taskName}
+                <button onClick={() => edit(item.id)}>Edit</button>
+                <button onClick={() => deleteHandler(item.id)}>Delete</button>
+              </ul>
+            </li>
+          );
+        })}
+      </>
     </div>
   );
 }
-
-export default App;
